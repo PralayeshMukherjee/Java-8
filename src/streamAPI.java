6d7524b054c77cb76fbd6282149720c27cc29a98
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2026-09-20 17:08:46 +0530

    find the count and use case of stream

--- src/streamAPI.java
+++ src/streamAPI.java
@@ -1,8 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class streamAPI {
     public static void main(String[] args){
         List<Integer> list = Arrays.asList(3,5,6,4,1,10);
+        list.forEach(n->System.out.print(n+" "));
+        Stream<Integer> stream = list.stream();
+        stream.forEach(n-> System.out.print(n+" "));
+        Stream<Integer> countN = list.stream();
+        long count = countN.count();
+        System.out.println(count);
     }
 }
