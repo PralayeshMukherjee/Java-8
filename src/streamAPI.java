cff555cf1e9215d03989749e9acfb53340e038a9
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2026-09-20 15:23:34 +0530

    create the list of integer

--- src/streamAPI.java
+++ src/streamAPI.java
@@ -1,2 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class streamAPI {
+    public static void main(String[] args){
+        List<Integer> list = Arrays.asList(3,5,6,4,1,10);
+    }
 }
