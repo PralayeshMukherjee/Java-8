e5ef741ee945a7363dbda32b7758948d440a6bd2
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2026-09-21 14:46:28 +0530

    add a little documentation

--- src/lamdaExpression.java
+++ src/lamdaExpression.java
@@ -1,2 +1,14 @@
+
+//  @FunctionalInterface
+//  interface A{
+//      void show();
+//  }
+//  class B implements A{
+//        @Override
+//      public void show(){
+//            System.out.println("Hii");
+//      }
+//  }
+
 public class lamdaExpression {
 }
