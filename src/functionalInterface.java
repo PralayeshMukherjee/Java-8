ab5e6af0c5fb431bcd73d2cc32a4be9321b4b40e
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2026-09-21 14:30:10 +0530

    create the interface of A with 1 function and a new class called B which implement the A and override the show method

--- src/functionalInterface.java
+++ src/functionalInterface.java
@@ -1,2 +1,17 @@
+@FunctionalInterface
+interface A{
+    void show();
+//    void raj();
+// Functional Interface support only 1 method inside it without implementation
+}
+class B implements A{
+    @Override
+    public void show(){
+        System.out.println("Hii");
+    }
+}
 public class functionalInterface {
+    public static void main(String[] args) {
+
+    }
 }
