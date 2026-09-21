5561faaab270c49455d7a8e6d7e9c7880764f1f9
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2026-09-21 14:42:41 +0530

    add a little documentation

--- src/functionalInterface.java
+++ src/functionalInterface.java
@@ -5,13 +5,17 @@ interface A{
 // Functional Interface support only 1 method inside it without implementation
 }
 class B implements A{
-    @Override
+    @Override // you can use override if you want to do else
+    // if you don't want to use it then its ok because implementation means you
+    // have to write the body of the method that you define in interface
+    // use override annotation only for check
     public void show(){
         System.out.println("Hii");
     }
 }
 public class functionalInterface {
     public static void main(String[] args) {
-
+        B obj = new B();
+        obj.show();
     }
 }
