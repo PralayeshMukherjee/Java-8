c1e51b66b3f228c29bc554e854bf7d5c15d5864f
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2026-09-21 15:01:25 +0530

    add  a little documentation about lamda expression

--- src/lamdaExpression.java
+++ src/lamdaExpression.java
@@ -12,6 +12,10 @@ interface C{
 
 public class lamdaExpression {
     public static void main(String[] args) {
-
+        C obj = () -> {
+            System.out.println("hii");
+        };
+        obj.show(); // without creating the implementation class of the interface
+        // we can directly define the method body when we use using lamda expresion
     }
 }
